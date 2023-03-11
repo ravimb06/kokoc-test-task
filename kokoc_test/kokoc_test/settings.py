6e1e61846fdb0dcca9986d8ccdec4606abe99152
Ali Bogatyrev <marksheyder06@gmail.com>
2023-03-11 13:33:15 +0300

    Created applications: Survey and Profiles

--- kokoc_test/kokoc_test/settings.py
+++ kokoc_test/kokoc_test/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'profiles.apps.ProfilesConfig',
+    'survey.apps.SurveyConfig',
 ]
 
 MIDDLEWARE = [
