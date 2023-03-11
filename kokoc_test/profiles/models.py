5ccf1d75f105436e234c4a366c6765b211e73db0
Ali Bogatyrev <marksheyder06@gmail.com>
2023-03-11 16:41:22 +0300

    feat: Added user's model

--- kokoc_test/profiles/models.py
+++ kokoc_test/profiles/models.py
@@ -1,3 +1,44 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    """Модель пользователей."""
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        verbose_name='Имя пользователя'
+    )
+    first_name = models.CharField(
+        max_length=150,
+        blank=True,
+        verbose_name='Имя',
+    )
+    last_name = models.CharField(
+        max_length=150,
+        blank=True,
+        verbose_name='Фамилия',
+    )
+    email = models.EmailField(
+        max_length=254,
+        unique=True,
+        verbose_name='email',
+    )
+    scores = models.IntegerField(
+        default=0,
+        verbose_name="Колличество очков",
+    )
+    coins = models.IntegerField(
+        default=0,
+        verbose_name="Колличество монет",
+    )
+
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+    USERNAME_FIELD = 'email'
+
+    class Meta:
+        ordering = ('id',)
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return str(self.username)
